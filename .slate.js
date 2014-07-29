66798444d0ef75ca5df9d50c18007264a781314b
Brad Cerasani <bradcerasani@gmail.com>
2014-07-28 22:45:25 -0700

    slate tweaks

--- .slate.js
+++ .slate.js
@@ -1,11 +1,11 @@
 var pushRight = slate.operation("push", {
   "direction": "right",
-  "style": "bar-resize:screenSizeX/3"
+  "style": "bar-resize:screenSizeX/2"
 });
 
 var pushLeft = slate.operation("push", {
   "direction": "left",
-  "style": "bar-resize:screenSizeX/3"
+  "style": "bar-resize:screenSizeX/2"
 });
 
 var fullscreen = slate.operation("move", {
@@ -18,4 +18,12 @@ var fullscreen = slate.operation("move", {
 slate.bind("1:ctrl", function(win) {
   win.doOperation(fullscreen);
   var appName = win.app().name();
+});
+
+slate.bind("2:ctrl", function(win) {
+  win.doOperation(pushLeft);
+});
+
+slate.bind("3:ctrl", function(win) {
+  win.doOperation(pushRight);
 });
